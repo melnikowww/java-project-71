c0093523a01a149735ecad2190115d4ddcd0207b
melnikowww <melnikoffspb@ya.ru>
2023-03-18 20:29:40 +0300

    step five

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,35 @@
 package hexlet.code;
+import picocli.CommandLine;
+import picocli.CommandLine.*;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import java.io.File;
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff",
+    mixinStandardHelpOptions = true,
+    description = "Compares two configuration files and shows a difference.",
+    headerHeading="\n")
+class App implements Callable<Integer> {
+
+    @Parameters(paramLabel = "filepath1", index = "0", description = "path to first file.")
+    private String filepath1;
+    @Parameters(paramLabel = "filepath2", index = "1", description = "path to second file.")
+    private String filepath2;
+
+    @Option(names = { "-f", "--format" }, paramLabel = "format", description = "output format [default: stylish]")
+    File archive;
+    @Override
+    public Integer call() throws Exception{
+        try {
+            System.out.println(Differ.generate(filepath1, filepath2));
+        } catch (Exception e) {
+            System.out.println("Нет файлика то такого...");
+        }
+
+        return 0;
+    }
+    public static void main(String... args) throws Exception{
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,61 @@
+package hexlet.code;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+
+public class Differ {
+    public static String generate(String fpath1, String fpath2) throws Exception {
+
+        String content = Files.readString(makePath(fpath1));
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, String> map1 = objectMapper.readValue(content, new TypeReference<>() {
+        });
+
+        String content2 = Files.readString(makePath(fpath2));
+        ObjectMapper objectMapper1 = new ObjectMapper();
+        Map<String, String> map2 = objectMapper1.readValue(content2, new TypeReference<>() {
+        });
+
+        List<String> sortedKeys = new ArrayList<>(map1.keySet());
+        for (String item:map2.keySet()) {
+            if (!sortedKeys.contains(item)) {
+                sortedKeys.add(item);
+            }
+        }
+        Collections.sort(sortedKeys);
+
+        List<String> result = new ArrayList<>();
+        for (String item: sortedKeys) {
+            if (!map2.containsKey(item)) {
+                result.add(" - " + item + ": " + map1.get(item));
+            } else {
+                if (map1.containsKey(item)) {
+                    if (!map1.get(item).equals(map2.get(item))) {
+                        result.add(" - " + item + ": " + map1.get(item) + "\n" + " + " + item + ": " + map2.get(item));
+                    } else {
+                        result.add("   " + item + ": " + map2.get(item));
+                    }
+                }
+            }
+            if (!map1.containsKey(item)) {
+                result.add(" + " + item + ": " + map2.get(item));
+            }
+        }
+        return "{" + "\n" + String.join("\n", result) + "\n" + "}";
+    }
+
+    public static Path makePath(String fpath) throws Exception {
+        Path path = Paths.get(fpath).toAbsolutePath().normalize();
+        if (!Files.exists(path)) {
+            throw new Exception("File '" + path + "' does not exist!");
+            }
+        return path;
+    }
+}
+
+
