1c0789ba62515fe28bdca308b26ef7652d87de4a
melnikowww <melnikoffspb@ya.ru>
2023-03-20 22:25:10 +0300

    step six

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -14,22 +14,13 @@ public class DifferTest {
     public void diffNotEmptyTest() throws Exception {
         String filepath1 = "src/test/resources/notEmptyFile1.json";
         String filepath2 = "src/test/resources/notEmptyFile2.json";
-        assertEquals("{\n   follow: false\n"
-            + " - host: hexlet.io\n"
-            + " + host: hexlet.iooo\n"
-            + "   proxy: 123.234.53.22\n"
-            + "   timeout: 50\n}", Differ.generate(filepath1, filepath2));
+        assertEquals("{\n   follow: false\n - host: hexlet.io\n + host: hexlet.iooo\n   proxy: 123.234.53.22\n   timeout: 50\n}", Differ.generate(filepath1, filepath2));
     }
     @Test
     public void diffNotEmptyTest1() throws Exception {
         String filepath1 = "src/test/resources/emptyFile.json";
         String filepath2 = "src/test/resources/notEmptyFile1.json";
-        assertEquals("{\n"
-            + " + follow: false\n"
-            + " + host: hexlet.iooo\n"
-            + " + proxy: 123.234.53.22\n"
-            + " + timeout: 50\n"
-            + "}", Differ.generate(filepath1, filepath2));
+        assertEquals("{\n + follow: false\n + host: hexlet.io\n + proxy: 123.234.53.22\n + timeout: 50\n}", Differ.generate(filepath1, filepath2));
     }
 }
 
