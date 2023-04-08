446482db4864740802c19817c27094064cfc8954
melnikowww <melnikoffspb@ya.ru>
2023-04-08 13:39:24 +0300

    third fix

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -9,15 +9,17 @@ public class Differ {
     public static String generate(String fpath1, String fpath2, String format) throws Exception {
         Path path1 = makePath(fpath1);
         Path path2 = makePath(fpath2);
+        String extension1 = fpath1.substring(fpath1.lastIndexOf("."));
+        String extension2 = fpath1.substring(fpath1.lastIndexOf("."));
 
-        Map<String, Object> dataMap1 = Parser.parse(Files.readString(path1));
-        Map<String, Object> dataMap2 = Parser.parse(Files.readString(path2));
+        Map<String, Object> dataMap1 = Parser.parse(Files.readString(path1), extension1);
+        Map<String, Object> dataMap2 = Parser.parse(Files.readString(path2), extension2);
 
         Map<String, Map<String, Object>> resultMap = GenDiff.genDiff(dataMap1, dataMap2);
         return Formatter.chooseFormat(resultMap, format);
     }
     public static String generate(String fpath1, String fpath2) throws Exception {
-        return generate(fpath1, fpath2, "plain");
+        return generate(fpath1, fpath2, "stylish");
     }
     public static Path makePath(String fpath) throws Exception {
         Path path = Paths.get(fpath).toAbsolutePath().normalize();
--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -2,11 +2,24 @@ package hexlet.code;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+
+import java.util.HashMap;
 import java.util.Map;
 
 public class Parser {
-    public static Map<String, Object> parse(String content) throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.readValue(content, new TypeReference<>() { });
+    public static Map<String, Object> parse(String content, String dataFormat) throws Exception {
+        switch (dataFormat) {
+            case ".json" -> {
+                ObjectMapper objectMapper = new ObjectMapper();
+                return objectMapper.readValue(content, new TypeReference<>() { });
+            }
+            case ".yml" -> {
+                ObjectMapper objectMapper = new YAMLMapper();
+                return objectMapper.readValue(content, new TypeReference<>() { });
+            }
+            default -> System.out.println("Unknown extension");
+        }
+        return new HashMap<>();
     }
 }
