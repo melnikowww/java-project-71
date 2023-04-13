b1278633c165d38e50d0328ac8b5f984a014990d
melnikowww <melnikoffspb@ya.ru>
2023-04-13 11:37:06 +0300

    fourth fix

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -30,7 +30,7 @@ public class DifferTest {
     }
     @ParameterizedTest
     @ValueSource(strings = {"json", "yml"})
-    public void generateTest1(String format) throws Exception {
+    public void generateTest(String format) throws Exception {
         String filePath1 = "src/test/resources/file1." + format;
         String filePath2 = "src/test/resources/file2." + format;
 
